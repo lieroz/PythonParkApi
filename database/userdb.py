fa0b04ca2010ad2e7411dc751dd54bb8c00f64e7
lieroz <a.kamakin@icloud.com>
2017-06-08 15:37:56 +0300

    Fully implemented user.

--- database/userdb.py
+++ database/userdb.py
@@ -9,6 +9,19 @@ GET_USER_SQL = """SELECT about, email, fullname, nickname
 					FROM users WHERE nickname = %(nickname)s OR email = %(email)s"""
 
 
+def update_user_sql(user):
+	sql = 'UPDATE users SET'
+	if 'about' in user:
+		sql += ' about = %(about)s,'
+	if 'email' in user:
+		sql += ' email = %(email)s,'
+	if 'fullname' in user:
+		sql += ' fullname = %(fullname)s,'
+	sql = sql[:-1]
+	sql += ' WHERE nickname = %(nickname)s RETURNING about, email, fullname, nickname'
+	return sql
+
+
 class UserDbManager:
 	@staticmethod
 	def create(user):
@@ -24,9 +37,9 @@ class UserDbManager:
 			if connection:
 				connection.rollback()
 			code = status_codes['CONFLICT']
-			cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+			cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 			cursor.execute(GET_USER_SQL, {'nickname': user['nickname'], 'email': user['email']})
-			user = cursor.fetchone()
+			user = cursor.fetchall()
 		except psycopg2.DatabaseError as e:
 			print('Error %s' % e)
 			if connection:
@@ -45,7 +58,7 @@ class UserDbManager:
 		code = status_codes['OK']
 		try:
 			connection = psycopg2.connect(connection_string)
-			cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+			cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 			cursor.execute(GET_USER_SQL, {'nickname': nickname, 'email': None})
 			user = cursor.fetchone()
 			if user is None:
@@ -59,4 +72,22 @@ class UserDbManager:
 
 	@staticmethod
 	def update(user):
-		return user, status_codes['OK']
+		connection = None
+		code = status_codes['OK']
+		try:
+			connection = psycopg2.connect(connection_string)
+			cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+			cursor.execute(update_user_sql(user), user)
+			connection.commit()
+			if user is None:
+				code = status_codes['CONFLICT']
+		except psycopg2.DatabaseError as e:
+			print('Error %s' % e)
+			if connection:
+				connection.rollback()
+			code = status_codes['NOT_FOUND']
+			user = None
+		finally:
+			if connection:
+				connection.close()
+		return user, code
